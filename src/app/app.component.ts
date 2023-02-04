874329456bc50991b43b65a3a995c22ee47843a1
JuaniCaste23 <juanicaste23@gmail.com>
2023-02-03 21:48:35 -0300

    arreglando bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-coderhouse-tp4';
+  title = 'AngularCODERHOUSE-EntregaFinal';
 }
